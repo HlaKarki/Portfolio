f62ec5eb1a3b03af21771634d2fc680c11d514e5
Hla Htun <kshap.karki@gmail.com>
2023-04-08 12:20:52 -0700

    added personal information along with image and buttons

--- src/components/introPersonal/introPersonal.js
+++ src/components/introPersonal/introPersonal.js
@@ -3,8 +3,14 @@ import './introPersonal.css'
 
 const IntroPersonal = () => {
     return (
-        <div>
-            <p>Hello</p>
+        <div className="infos">
+            <div className="introNameSE">
+                <h1>Hi, I'm Hla Htun</h1>
+                <h1>Software Engineer</h1>
+            </div>
+
+            <h2>Add breakpoints to your blank page, then drop sections to have them responsive out of the box</h2>
+            <button id="myPortfolioButton">Portfolio Page</button>
         </div>
     )
 }
