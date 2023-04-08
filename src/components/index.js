3418f9861848fe4c7d1b45f4051740d4aa22c3c2
Hla Htun <kshap.karki@gmail.com>
2023-04-08 12:20:09 -0700

    exported footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from './navbar/navbar'
 export { default as IntroPersonal } from './introPersonal/introPersonal'
 export { default as Image } from './image/image'
+export { default as Footer } from './footer/footer'
