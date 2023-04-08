51692703e9b59843aa38622eb2a8c55f62f82ecb
Hla Htun <kshap.karki@gmail.com>
2023-04-08 13:36:54 -0700

    projects page set up

---
+++ src/pages/projectsPage/projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './projects.css'
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>This is my project page</h1>
+        </div>
+    )
+}
+
+export default Projects
