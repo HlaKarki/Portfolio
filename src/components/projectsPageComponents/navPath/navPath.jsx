31eeef4146ee883efe4b90f4b6fc8b2ca81b68d8
Hla Htun <kshap.karki@gmail.com>
2023-04-11 16:56:12 -0700

    navigation path component set up

---
+++ src/components/projectsPageComponents/navPath/navPath.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navPath.css'
+
+const NavPath = () => {
+    return (
+        <div className="navPathLinks">
+            <a href="/" title="Go to Hla's homepage">> Hla</a> /
+            <a href="/projects"> Projects</a>
+        </div>
+    )
+}
+
+
+export default NavPath
