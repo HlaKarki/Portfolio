54f517ab01108b2ceacb8a6400ac735881e9f2d0
Hla Htun <kshap.karki@gmail.com>
2023-06-02 19:50:12 -0700

    navigating using conventional react methods

--- src/components/projectsPageComponents/navPath/navPath.jsx
+++ src/components/projectsPageComponents/navPath/navPath.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './navPath.css'
+import {Link} from "react-router-dom";
 
 const NavPath = () => {
     return (
         <div className="navPathLinks">
-            <a href="/" title="Go to Hla's homepage">> Hla</a> /
-            <a href="/projects"> Projects</a>
+            <Link to="/">
+                <span title="Go to Hla's homepage">> Hla</span> /
+            </Link>
+            <Link to="/projects">
+                <span> Projects</span>
+            </Link>
         </div>
     )
 }
