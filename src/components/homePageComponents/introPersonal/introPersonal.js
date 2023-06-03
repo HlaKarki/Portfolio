a43861a4ae97ecfd7390f8d68758bead86937f06
Hla Htun <kshap.karki@gmail.com>
2023-06-02 19:49:09 -0700

    new project addition

--- src/components/homePageComponents/introPersonal/introPersonal.js
+++ src/components/homePageComponents/introPersonal/introPersonal.js
@@ -3,7 +3,7 @@ import './introPersonal.css'
 import { Link } from 'react-router-dom'
 
 const IntroPersonal = () => {
-    const personalDescription = "As a dedicated and curious individual with a passion for problem-solving, I am constantly seeking new challenges that allow me to apply my skills and expand my knowledge. With a strong foundation in programming languages such as JavaScript, C++, Java, and Python, I have honed my abilities in front-end development, back-end development, and UX design. I am a detail-oriented and collaborative team player, with experience working on projects that require both technical expertise and creativity. I am eager to apply my skills and knowledge in a professional setting and contribute to the success of any team I work with and will continue to strive towards exceeding expectations."
+    const personalDescription = "As a dedicated and curious individual with a passion for problem-solving, I am constantly seeking new challenges that allow me to apply my skills and expand my knowledge. With a strong foundation in programming languages such as JavaScript, C++, Java, and Python, and as well as lower level language such as MASM Assembly (IA32), I have honed my abilities in front-end development, back-end development, and UX design. I am a detail-oriented and collaborative team player, with experience working on projects that require both technical expertise and creativity. I am eager to apply my skills and knowledge in a professional setting and contribute to the success of any team I work with and will strive towards exceeding expectations."
     return (
         <div className="infos">
             <div className="introNameSE">
