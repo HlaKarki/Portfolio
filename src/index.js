c167f772a7d38be5dab8ee15147bc8fa1d5cf3a4
Hla Htun <kshap.karki@gmail.com>
2024-01-29 01:04:25 -0800

    starting anew - vite + gsap set up

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-
-import App from './App'
-import './index.css'
-
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
-
-root.render(<App />);
