07e484ff133041892ce678ba8c046483c310331a
Hla Htun <kshap.karki@gmail.com>
2024-04-02 12:41:23 -0700

    feat: revamping based on YT ByteGrad

--- src/components/homePageComponents/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from './navbar/navbar'
-export { default as IntroPersonal } from './introPersonal/introPersonal'
-export { default as Image } from './image/image'
-export { default as Footer } from './footer/footer'
--- src/components/projectsPageComponents/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Header } from './header/header'
-export { default as ProjectsContent } from './projects/projects'
-export { default as NavPath } from './navPath/navPath'
