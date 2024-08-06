6d02869159ee522d8488161e0188d89397d1a9fb
hlakarki <kshap.karki@gmail.com>
2024-08-05 21:10:24 -0700

    feat: navbar revamp

--- src/components/darkMode/toggleButton.tsx
+++ src/components/darkMode/toggleButton.tsx
@@ -5,40 +5,29 @@ import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 interface ThemeToggleProps {
     className?: string;
 }
 
 export function ThemeToggle({ className }: ThemeToggleProps) {
-    const { setTheme } = useTheme()
+    const { setTheme, theme } = useTheme()
+    const [mounted, setMounted] = React.useState<boolean>(false)
 
+    React.useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon" className={className + " border border-black dark:border-gray-300"}>
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">Toggle theme</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="center" className={className}>
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                    System
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Button variant={"ghost"} onClick={() => setTheme(theme==="dark" ? "light" : "dark")}>
+            {
+                theme && theme === "dark" ?
+                <Sun className={""}/> :
+                <Moon className={"w-5"}/>
+            }
+        </Button>
     )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,18 +8,20 @@ import {MenuBar} from "@/components/menuBar";
 
 export default function Header() {
     return (
-        <nav className="fixed top-0 left-0 right-0 z-50 bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg dark:bg-black dark:bg-opacity-30">
+        <nav className={"fixed top-[0.5vh] left-0 right-0 z-50 bg-light-background_support shadow-xl bg-opacity-20 backdrop-filter backdrop-blur-lg "
+                        + " dark:bg-black dark:bg-opacity-30 md:w-[75%] md:mx-auto md:rounded-full  "
+                        + " transition duration-300 ease-in-out md:hover:scale-102 "}>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     {/* Left section: Navigation links (hidden on mobile) */}
                     <div className="hidden md:flex flex-1 items-center justify-start space-x-4">
-                        <Link href="/" className="text-gray-800 dark:text-white hover:text-primary-light transition-colors duration-300">
+                        <Link href="/" className="text-gray-800 dark:text-white hover:text-primary-light transition duration-300 ease-in-out hover:scale-110">
                             Home
                         </Link>
-                        <Link href="/#projects" className="text-gray-800 dark:text-white hover:text-primary-light transition-colors duration-300">
+                        <Link href="/#projects" className="text-gray-800 dark:text-white hover:text-primary-light transition duration-300 ease-in-out hover:scale-110">
                             Projects
                         </Link>
-                        <Link href="/#tools" className="text-gray-800 dark:text-white hover:text-primary-light transition-colors duration-300">
+                        <Link href="/#tools" className="text-gray-800 dark:text-white hover:text-primary-light transition duration-300 ease-in-out hover:scale-110">
                             Tools
                         </Link>
                     </div>
